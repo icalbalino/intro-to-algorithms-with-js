fc39441e28b5e7e0876b5233876708fccc49b560
if0balino <balino.ical@gmail.com>
2021-11-25 09:09:29 +0700

    optimization with chacing, faster algorithm

---
+++ optimization-with-caching/index.js
@@ -0,0 +1,42 @@
+// Faster algorithms
+
+// quadratic
+// const isUnique = (arr) => {
+//   let result = true;
+//   for (let i = 0; i < arr.length; i++) {
+//     console.log(`~~~~ OUTER LOOP ~~~~ i === ${i}`);
+//     for (let j = 0; j < arr.length; j++) {
+//       console.log(`~~~~ INNER LOOP ~~~~ j === ${j}`);
+//       if (i !== j && arr[i] === arr[j]) {
+//         result = false;
+//       }
+//     }
+//   }
+//   return result;
+// };
+// console.log(isUnique([1, 2, 3]) === true);
+// console.log(isUnique([1, 1, 3]) === false);
+
+
+// linear
+// breadcrumbs method is keeping track of things that you've already seen. it's also called chacing.
+// with constant time operation
+// when we use the breadcrumbs technique we can take a quadratic time algorithm, and make it linear.
+const isUnique = (arr) => {
+  const breadcrumbs = {};
+  let result = true;
+  
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`~~~~ LOOP ~~~~ i === ${i}`);
+    if (breadcrumbs[arr[i]]) {
+      result = false;
+    } else {
+      breadcrumbs[arr[i]] = true;
+    }
+  }
+  
+  return result;
+};
+// console.log(isUnique([1, 2, 3]) === true);
+console.log(isUnique([1, 1, 3]) === false);
+
